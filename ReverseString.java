85c0e7aa9919460ec4fda66845af9ed5d4ad9518
Priyanka Pujar <priyankapujar721@gmail.com>
2025-01-04 17:58:10 +0530

    abd

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString {
+    public static void main(String[] args) {
+        String original = "Java";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+ 
+        System.out.println("Original String: " + original);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
